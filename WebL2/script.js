c780ce7a18befebf84582c5baaaca672baa58176
Katastorm <45739730+katastorm@users.noreply.github.com>
2020-01-11 16:14:40 +0100

    Stable pour rendu 1
    mise en place des points sur la carte
    separation javascript/html

--- WebL2/script.js
+++ WebL2/script.js
@@ -3,10 +3,156 @@
 
 
 $(window).scroll(function(e){
-  parallax();
+	parallax();
 });
 
 function parallax(){
-  var scrolled = $(window).scrollTop();
-  $('.background').css('top',-(scrolled*0.15)+'px');
+	var scrolled = $(window).scrollTop();
+	$('.background').css('top',-(scrolled*0.15)+'px');
 }
+
+
+
+var appInit = false;
+
+$(document).ready(function(){
+
+
+	//Partie carte :
+	var mymap = L.map('mapid').setView([48.835, 2.348], 8);
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		maxZoom: 18,
+		id: 'mapbox.streets',
+		accessToken: 'pk.eyJ1IjoianVsaWVuNDIwIiwiYSI6ImNrMzV5M21vejBuaW4zbnBlbjY4b205a2wifQ.HlGy-0kBMA6qU3tu-b2tTA'
+	}).addTo(mymap);
+
+	var markerGroup = L.layerGroup().addTo(mymap);
+
+/*
+	mymap.on('click', function(e) {
+	 AddMapMarker(e.latlng.lat, e.latlng.lng);
+	});*/
+
+
+
+//Utilitaires pour la carte
+function AddMapMarker(lat, long, name, url){
+	var marker =	L.marker([lat, long], {}).addTo(markerGroup);
+	marker.bindPopup(name + "<br><a target=\"_blank\" href="+url+">Acceder au site web</a>");
+	marker.on('click', onClickMarker);
+}
+
+function ClearMinimap(){
+	markerGroup.clearLayers();
+}
+
+function onClickMarker(e) {
+	var popup = e.target.getPopup();
+	var content = popup.getContent();
+	console.log(content);
+}
+
+
+
+
+
+
+
+	//Transition du boutton "Commencer mes recherches"
+	$("#searchButton").click(	SendRequest);
+	$('#searchZone').fadeOut(0);
+	$('#openSearchPannel').click(function(e){    
+		$('#beforeSearch').fadeOut('fast', function(){
+       // $('.searchZone').replace('<div id="beforeSearch"></div>').fadeIn('fast');
+       $('#searchZone').fadeIn('fast');
+   });
+	});
+
+
+	//Remplissage des choix des champs
+	function AddOptionElement(optionTag, facetToAdd){
+		theDiv = document.getElementById(optionTag);
+		var option = document.createElement("option");
+		option.value = facetToAdd["name"];
+		theDiv.appendChild( option);
+	}
+
+
+//Automatique, au démarage
+SendRequest();
+
+function SendRequest(){
+
+//Recuperation des critères de recherche
+var secteur = document.getElementById("search_sect").previousSibling.previousSibling.value;
+var diplome = document.getElementById("search_dip").previousSibling.previousSibling.value;
+var region = document.getElementById("search_reg").previousSibling.previousSibling.value;
+var niveau = document.getElementById("search_niv").previousSibling.previousSibling.value;
+
+var conditions  = (secteur=="")?"":"&refine.sect_disciplinaire_lib="+secteur;
+conditions += (diplome=="")?"":"&refine.diplome_lib="+diplome;
+conditions += (region =="")?"":"&refine.reg_etab_lib="+region;
+conditions += (niveau =="")?"":"&refine.niveau_lib="+niveau;
+
+//Envoie de la première requète
+$.getJSON("https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-principaux-diplomes-et-formations-prepares-etablissements-publics&rows=50&sort=-rentree_lib&facet=etablissement_lib&facet=niveau_lib&facet=diplome_lib&facet=gd_disciscipline_lib&facet=sect_disciplinaire_lib&facet=reg_etab_lib&facet=dep_ins_lib&facet=com_etab_lib&facet=com_etab"+conditions, function(data) {
+
+	ClearMinimap();
+
+		//En cas d'erreur
+		if(data == undefined)
+			return;
+
+		//Aucun résultats		
+		if(data["nhits"] == 0){
+			alert("Aucun résultat trouvé");
+			return;
+		}
+
+
+
+//Remplissage des champs automatiques (à faire seulement 1 fois au démarage)
+if(!appInit){
+	appInit = true;
+//"niveau_lib"
+jQuery.each(data["facet_groups"][6]["facets"], function() {
+	AddOptionElement("search_niv", this);
+});
+//"reg_etab_lib"
+jQuery.each(data["facet_groups"][5]["facets"], function() {
+	AddOptionElement("search_reg", this);
+});
+//"diplome_lib"
+jQuery.each(data["facet_groups"][7]["facets"], function() {
+	AddOptionElement("search_dip", this);
+});
+//"sect_disciplinaire_lib"
+jQuery.each(data["facet_groups"][2]["facets"], function() {
+	AddOptionElement("search_sect", this);
+});
+//"com_etab" == 8
+}
+
+
+
+//creation de la requete correllé pour avoir la géolocalisation de l'ecole
+var records = data["records"]; 
+let searchById = (records.length > 0)?"&q="+records[0]["fields"]["com_etab"]:"";
+for (var i=1 ; i <  records.length ; i++){
+	searchById += " OR " + records[i]["fields"]["com_etab"];
+}
+
+//Envoie de la seconde requête et placement des marqueurs
+$.getJSON("https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-principaux-etablissements-enseignement-superieur&rows=50"+searchById, function(etablissements) {
+	records = etablissements["records"]; 
+	for (var i=0 ; i <  records.length ; i++)
+	{
+		let pos = records[i]["geometry"]["coordinates"];
+		AddMapMarker(pos[1], pos[0], records[i]["fields"]["uo_lib"], records[i]["fields"]["url"]);
+	}
+});
+});
+}
+});
+
